77bd46b7679b8a2d9534b05a49a0359850f34587
Eliáš Cizl <cizl.elias@gmail.com>
2020-09-01 14:06:11 +0200

    Tests project

---
+++ WellDividedTests/DividerTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using WellDividedCore;
+
+namespace WellDividedTests
+{
+	[TestClass]
+	public class DividerTests
+	{
+		[TestMethod]
+		public void TestLoadDataCSV()
+		{
+			// default folder is WellDivided/WellDividedTests/bin/Debug/netcoreapp3.1
+			
+		}
+	}
+}
