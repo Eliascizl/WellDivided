f2e0a4b3925ec358e66e595bf130b7ee4b7b6081
Eliáš Cizl <cizl.elias@gmail.com>
2020-09-11 21:52:43 +0200

    General attribute evaluation

--- WellDividedCore/GeneralAttribute.cs
+++ WellDividedCore/GeneralAttribute.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using System.Text;
 
 namespace WellDividedCore
@@ -15,16 +17,49 @@ namespace WellDividedCore
 			{
 				for (int j = 0; j < groups[i].Elements.Count; j++)
 				{
-					if(instanceCounts[i].TryGetValue(groups[i].Elements[j].Attributes[this], out int value))
+					var attributeInstance = (GeneralAttributeInstance)groups[i].Elements[j].Attributes[this];
+					var attributeValue = attributeInstance.Value;
+					if (instanceCounts[i].TryGetValue(attributeValue, out int value))
 					{
-
+						instanceCounts[i][attributeValue] = value + 1;
 					} 
 					else
 					{
-
+						instanceCounts[i][attributeValue] = 1;
 					}
 				}
 			}
+
+			Dictionary<string, float> evaluatedInstances = new Dictionary<string, float>();
+			for (int i = 0; i < instanceCounts.Length; i++)
+			{
+				foreach(var key in instanceCounts[i].Keys)
+				{
+					if(!evaluatedInstances.ContainsKey(key))
+					{
+						evaluatedInstances[key] = EvaluateInstance(key, instanceCounts);
+					}
+				}
+			}
+
+			float totalScore = 0;
+			foreach (var instanceScore in evaluatedInstances.Values)
+			{
+				totalScore += instanceScore;
+			}
+			totalScore /= evaluatedInstances.Count;
+
+			return totalScore;
+		}
+
+		/// <summary>
+		/// Evaluates how much a given string is balanced - how much it appears throughout all groups, not just one.
+		/// </summary>
+		/// <param name="instanceValue">The current key to count appearances of.</param>
+		/// <param name="instanceCounts">The counts of the appearances in the particular groups.</param>
+		/// <returns>The balance score for the given key (instance). In the range of 0 to 1.</returns>
+		private float EvaluateInstance(string instanceValue, Dictionary<string, int>[] instanceCounts)
+		{
 
 			return 0f;
 		}
@@ -37,7 +72,7 @@ namespace WellDividedCore
 
 		public class GeneralAttributeInstance : AttributeInstance
 		{
-			public string Value { get; }
+			public string Value { get; internal set; }
 
 			public GeneralAttributeInstance(string value)
 			{
---
+++ WellDividedCore/Utility/IEnumerableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WellDividedCore.Utility
+{
+	static class IEnumerableExtensions
+	{
+		public static float StandardDeviation(this IEnumerable<float> source, float expectedValue)
+		{
+			var sum = 0f;
+			var count = 0;
+			foreach (var number in source)
+			{
+				sum += (number - expectedValue) * (number - expectedValue);
+				count++;
+			}
+
+			return (float)Math.Sqrt(sum / count);
+		}
+	}
+}
