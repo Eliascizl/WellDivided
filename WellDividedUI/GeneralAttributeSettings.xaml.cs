7a74e5dd5147e6fd101ee928dd2a3f2a610ecb80
Eliáš Cizl <cizl.elias@gmail.com>
2020-09-13 19:02:50 +0200

    General attribute uI

---
+++ WellDividedUI/GeneralAttributeSettings.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WellDividedCore;
+
+namespace WellDividedUI
+{
+	/// <summary>
+	/// Interaction logic for AttributeSettings.xaml
+	/// </summary>
+	public partial class GeneralAttributeSettings : UserControl
+	{
+		internal GeneralAttribute GeneralAttribute { get; private set; }
+
+		public GeneralAttributeSettings(GeneralAttribute generalAttribute)
+		{
+			InitializeComponent();
+
+			this.GeneralAttribute = generalAttribute;
+			attributeNameLabel.Content = generalAttribute.Name;
+
+		}
+
+
+		private void balancedCheckBox_Checked(object sender, RoutedEventArgs e)
+		{
+			importanceLabel.IsEnabled = true;
+			importanceSlider.IsEnabled = true;
+			importanceValue.IsEnabled = true;
+		}
+
+		private void balancedCheckBox_Unchecked(object sender, RoutedEventArgs e)
+		{
+			importanceLabel.IsEnabled = false;
+			importanceSlider.IsEnabled = false;
+			importanceValue.IsEnabled = false;
+		}
+	}
+}
